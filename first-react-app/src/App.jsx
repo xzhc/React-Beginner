57b6d40f356c2024e8ee75889882ea00c2845895
xzhc <1838998336@qq.com>
2025-04-16 10:08:59 +0800

    JSX Basics

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -1,9 +1,16 @@
 function App() {
+  const myCustomLabel = <label htmlFor="inputId">sdsdd</label>;
   return (
-    <div>
-      <h1>First React App</h1>
+    <div className="large" id="largeDiv">
+      {myCustomLabel}
+      <input id="inputId" type="number" defaultValuevalue={3} />
     </div>
   );
 }
 
 export default App;
+
+// div with the class large and the id largeDiv with text Hi.
+// Remove the text Hi
+// Add a label with the for prop set to inputId and any text
+// Add an input with the id inputId, type number, and value of 3 as a number
