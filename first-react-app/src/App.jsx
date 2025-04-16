1d46ad46c518547edb4ea56de5bdf038863a9079
xzhc <1838998336@qq.com>
2025-04-16 21:08:41 +0800

    Input Event Listeners

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -1,37 +1,17 @@
-// import { useState } from "react";
-import { AppClass } from "./AppClass";
-import { CounterClass } from "./CounterClass";
-// function slowGetter() {
-//   console.log("State getter");
-//   return "xzh";
-// }
+import { useState } from "react";
 
 function App() {
-  // const [name, setName] = useState("xzh");
-  // const [age, setAge] = useState(25);
-  // const [person, setPerson] = useState({ name: "xzh", age: 25 });
-  // function handleClick() {
-  //   // console.log("Age:", age);
-  //   //setName("xsy");
-  //   //setAge((currentAge) => currentAge + 1);
-  //   //console.log("Age:", age);
-  //   //setAge((currentAge) => currentAge + 1);
-  //   setPerson({ ...person, name: "xsy" });
-  // }
+  const [name, setName] = useState("xzh");
+
   return (
-    // <h1 onClick={handleClick}>
-    //   {person.name} {person.age}
-    // </h1>
-    // <Counter />
-    <>
-      <AppClass />
-      <CounterClass />
-    </>
+    <input
+      type="text"
+      value={name}
+      onChange={(e) => {
+        setName(e.target.value);
+      }}
+    />
   );
 }
 
 export default App;
-
-//Create a new component called counter
-//have a state for a counter that starts at 0 and render that in the jsx
-//When you click the number increment it by 1
