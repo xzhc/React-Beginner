c0631e9cd3d62bd27795309cd0338f163a372c20
xzhc <1838998336@qq.com>
2025-04-19 13:23:08 +0800

    finish 03-name-counter

---
+++ 03-name-counter/src/App.jsx
@@ -0,0 +1,13 @@
+import { ClassComponment } from "./ClassComponent";
+import { FunctionComponent } from "./FunctionComponent";
+
+function App() {
+  return (
+    <>
+      <ClassComponment />
+      {/* <FunctionComponent /> */}
+    </>
+  );
+}
+
+export default App;
---
+++ 03-name-counter/src/ClassComponent.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+export class ClassComponment extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      age: 0,
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          value={this.state.name}
+          onChange={(e) => this.setState({ name: e.target.value })}
+        />
+        <br />
+        <br />
+        <button
+          onClick={() =>
+            this.setState((currentState) => {
+              return { age: currentState.age - 1 };
+            })
+          }
+        >
+          -
+        </button>
+        {this.state.age}
+        <button
+          onClick={() =>
+            this.setState((currentState) => {
+              return {
+                age: currentState.age + 1,
+              };
+            })
+          }
+        >
+          +
+        </button>
+        <br />
+        <br />
+        My name is {this.state.name}, and I am {this.state.age} years old.
+      </div>
+    );
+  }
+}
