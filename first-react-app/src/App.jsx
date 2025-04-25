1750523ee5ca34187336d7cd7ad12e0c9313ac5f
xzhc <1838998336@qq.com>
2025-04-26 06:53:35 +0800

    Rendering Lists

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -1,10 +1,26 @@
-import { ClassChild } from "./ClassChild";
-import { Child } from "./Child";
+import { useState } from "react";
 
 function App() {
+  const [items, setItems] = useState([
+    { id: crypto.randomUUID(), name: "Item1" },
+    { id: crypto.randomUUID(), name: "Item2" },
+  ]);
+  function addItem() {
+    setItems((currentItems) => {
+      return [...currentItems, { id: crypto.randomUUID(), name: "New Item" }];
+    });
+  }
   return (
     <div>
-      <Child favoriteNumber={1} />
+      <button onClick={addItem}>Add Item</button>
+      <pre>
+        {items.map((item) => (
+          <div key={item.id}>
+            {item.name}
+            <input type="text" />
+          </div>
+        ))}
+      </pre>
     </div>
   );
 }
