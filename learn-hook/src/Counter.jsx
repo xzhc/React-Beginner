eac94e9a7ba33e4e82706597a78983d8b7d9f1dc
xzhc <1838998336@qq.com>
2025-05-26 14:53:23 +0800

    achieve counter by useState

---
+++ learn-hook/src/Counter.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+export function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Counter: {count}</h1>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+      <button onClick={() => setCount(0)}>Reset</button>
+    </>
+  );
+}
